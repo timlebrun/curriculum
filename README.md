f02404e6a887e993e9f6e1794845bed37aafc00b
timlebrun <Dropthebass45>
2019-04-19 23:30:58 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Curriculum
+
+This is my Curriculum Vitae.
+
+It is entirely developped in HTML5 and CSS3.
+
+The purpose of this script is to export it to PDF, using a headless browser.
+
+## Build It Yourself ?
+```
+npm install
+npm run export
+```
+The result of conversion is saved in the `dist` directory, right there :arrow_forward:
+
+## License
+WTFPL 2.0 (please don't use my personnal data)
---
+++ curriculum.js
@@ -0,0 +1,19 @@
+const puppeteer = require('puppeteer');
+const { join } = require('path');
+
+const curriculum = join(__dirname, 'src/index.html');
+
+(async () => {
+  console.log('Launching Headless Browser...');
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  console.log('Loading template...');
+  await page.goto('file:///' + curriculum, { waitUntil: 'networkidle2' });
+
+  console.log('Exporting as a PDF file...');
+  await page.pdf({ path: 'dist/curriculum.pdf', format: 'A4' });
+
+  console.log('Done !')
+  await browser.close();
+})();
