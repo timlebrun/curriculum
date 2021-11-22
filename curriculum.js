d3e104b500b2ec9e1607eb3b5b9e5966000ea4b4
Tim Lebrun <tim.lebrun@airweb.fr>
2021-11-22 23:08:34 +0100

    :arrow_up: Upgrading to pupeteer v11

--- curriculum.js
+++ curriculum.js
@@ -5,14 +5,14 @@ const curriculum = join(__dirname, 'src/index.html');
 
 (async () => {
   console.log('Launching Headless Browser...');
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   console.log('Loading template...');
   await page.goto('file:///' + curriculum, { waitUntil: 'networkidle2' });
 
   console.log('Exporting as a PDF file...');
-  await page.pdf({ path: 'dist/curriculum.pdf', format: 'A4' });
+  await page.pdf({ path: 'dist/curriculum.pdf', format: 'A4', pageRanges: '1' });
 
   console.log('Done !')
   await browser.close();
